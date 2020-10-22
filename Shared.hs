6baa11b7dd9b3bbf151827f9f6afc5ffcbfe9b97
Igor Belousov <lipranu@gmail.com>
2020-10-23 02:13:35 +0700

    Updated shared module to chapter 1.4

--- Shared.hs
+++ Shared.hs
@@ -43,8 +43,43 @@ instance (GEq a, GEq b) => GEq (Either a b) where
 instance (GEq a, GEq b) => GEq (a, b) where
   geq (x1, y1) (x2, y2) = geq x1 x2 && geq y1 y2
 
+class GEnum a where
+  genum :: [a]
+
+data Colour
+  = Red
+  | Green
+  | Blue
+  deriving Show
+
+instance Generic Colour where
+  type Rep Colour = Either () (Either () ())
+
+  from Red   = Left ()
+  from Green = Right $ Left ()
+  from Blue  = Right $ Right ()
+
+  to (Left _)          = Red
+  to (Right (Left _))  = Green
+  to (Right (Right _)) = Blue
+
+instance GEq () where
+  geq () () = True
+
+instance Eq Colour where
+  (==) = eq
+
+instance GEnum () where
+  genum = [()]
+
+instance (GEnum a, GEnum b) => GEnum (Either a b) where
+  genum = (Left <$> genum) ++ (Right <$> genum)
+
 eq :: (Generic a, GEq (Rep a)) => a -> a -> Bool
 eq x y = geq (from x) (from y)
+
+enum :: (Generic a, GEnum (Rep a)) => [a]
+enum = to <$> genum
 
 tree1, tree2 :: Tree Integer
 tree1 = Node (Leaf 1) (Leaf 2)
