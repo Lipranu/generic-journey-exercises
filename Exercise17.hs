17ae2c1563d67299d1f7cf071476df99d900c6b6
Igor Belousov <lipranu@gmail.com>
2020-10-27 11:42:46 +0700

    Completed exercise 17

---
+++ Exercise17.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE GADTs #-}
+
+module Exercise17 where
+
+import Chapter2_10
+
+mapProduct :: SList c xs
+           -> (forall c x . f x -> g x)
+           -> Product f xs
+           -> Product g xs
+mapProduct (SCons rs) op (Cons x xs) = Cons (op x) $ mapProduct rs op xs
+mapProduct SNil _ Nil = Nil
