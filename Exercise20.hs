d6fe5d15d621a2c6d3292ebafbb67287b8dbb43a
Igor Belousov <lipranu@gmail.com>
2020-10-28 21:48:04 +0700

    Completed exercise 20

---
+++ Exercise20.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Exercise20 where
+
+import Chapter2_12
+import Exercise17
+
+data Projection f xs x = MkProjection (Product f xs -> f x)
+
+projections :: forall c f xs . SList c xs -> Product (Projection f xs) xs
+projections (SCons rs) = Cons
+  (MkProjection (\(Cons x _) -> x))
+  (mapProduct rs shiftProjection (projections @c @f rs))
+projections SNil = Nil
+
+shiftProjection :: Projection f xs x -> Projection f (y : xs) x
+shiftProjection (MkProjection proj) = MkProjection (\(Cons _ xs) -> proj xs)
